6b1ce4aecd9c0e93ac5c0e5b53f105a7096ca785
Marcel Haerry <haerry@puzzle.ch>
2008-10-15 19:13:38 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+#
+# tomcat module
+#
+# Copyright 2008, Puzzle ITC
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+# modules_dir { \"tomcat\": }
+
+class tomcat {
+    include tomcat::base
+}
+
+class tomcat::base {
+    package{'tomcat':
+        ensure => present,
+    }
+    service{tomcat:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => Package[tomcat],
+    }
+}
