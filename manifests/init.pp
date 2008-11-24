b0d5062932eae17e15c280da1c55a30f730fe136
Marcel Haerry <haerry@puzzle.ch>
2008-11-24 18:00:02 +0100

    added sysconfig file to be managed by puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,10 +11,11 @@
 # the Free Software Foundation.
 #
 
-# modules_dir { \"tomcat\": }
-
 class tomcat {
-    include tomcat::base
+    case $operatingsystem {
+        centos: { include tomcat::centos }
+        default: { include tomcat::base }
+    }
 }
 
 class tomcat::base {
@@ -29,5 +30,16 @@ class tomcat::base {
         enable => true,
         hasstatus => true,
         require => Package['tomcat5'],
+    }
+}
+
+class tomcat::centos inherits tomcat::base {
+    file{'/etc/sysconfig/tomcat5':
+        source => [ "puppet://$server/files/tomcat/sysconfig/${fqdn}/tomcat5",
+                    "puppet://$server/files/tomcat/sysconfig/tomcat5",
+                    "puppet://$server/tomcat/sysconfig/${fqdn}/tomcat5" ],
+        require => Package['tomcat5'],
+        notify => Service['tomcat5'],
+        owner => root, group => 0, mode => 0644;
     }
 }
