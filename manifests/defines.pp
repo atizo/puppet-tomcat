5015c042335dd28281be39479cc0f604aa791bc2
Simon Josi <josi@puzzle.ch>
2008-11-26 17:43:52 +0100

    blame

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -8,6 +8,6 @@ define tomcat::vhost(
     concatenated_file_part{"tomcat_vhost_${name}":
         dir => "${TOMCAT_VHOSTSDIR}",
         content => template('tomcat/vhosts/vhost.erb'),
-        onwer => root, group => 0, mode => 0600;
+        owner => root, group => 0, mode => 0600;
     }
 }
